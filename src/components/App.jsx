b73c5167022886d77bc0be720854368a21a77bab
akahoshi1421 <akaho1421@gmail.com>
2022-11-30 17:26:59 +0900

    グラフの初期化処理の実装
    ページ読み込み時にChart.jsを読み込んで表示させるところまで

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,12 @@ import { CheckBoxs } from "./CheckBoxs";
 
 export const App = () => {
   const END_POINT = "https://opendata.resas-portal.go.jp";
+  const KEY = "sXkAFcfVBW9zVWIINTnMyFZGr9PdXOQ9vdFVFl5K";
 
   return (
     <>
       <h1>人口推移グラフ</h1>
-      <div className="populationGraph">
-
-      </div>
-      <CheckBoxs endPoint={END_POINT}/>
+      <CheckBoxs END_POINT={END_POINT} API_KEY={KEY}/>
     </>
   );
 }
--- src/components/CheckBoxs.jsx
+++ src/components/CheckBoxs.jsx
@@ -1,20 +1,66 @@
+import { Chart } from "chart.js";
 import { useEffect, useState } from "react"
 
+let myChart;
+
 export const CheckBoxs = (props) =>{
+    const {END_POINT, API_KEY} = props;
 
     const [prefs, setPrefs] = useState([]);
+    const [populations, setPopulations] = useState({});
+
+    //ヘッダ設定
+    const setHeader = key => {
+        const headers = new Headers();
+        headers.set("X-API-KEY", key);
+        return headers;
+    }
+
     //グラフ描画処理
     const showGraph = code => {
-        alert(code);
+        const headers = setHeader(API_KEY);
+        const URL = `${END_POINT}/api/v1/population/composition/perYear?prefCode=${code}`;
+
+        fetch(URL, {headers: headers}).then(res => {
+            return res.json();
+        })
+        .then(json => {
+            const thePref = json.result.data[0].data;
+
+            //チェックをしたのか外したのかを判定
+            if(code in populations){
+                //消す処理
+                const newPopulations = Object.create(populations);
+                delete newPopulations[code];
+                setPopulations(newPopulations);
+            }
+            else{
+                //追加処理
+                const newPopulations = Object.create(populations);
+                newPopulations[code.toString()] = thePref;
+                setPopulations(newPopulations);
+            }
+
+            const graph = document.querySelector("#drawGraph").getContext("2d");
+            // const myChart = new Chart( graph, {
+            //         type: "line",
+            //         data: {
+            //             labels : newPopulations["4"].filter(name => {
+            //                 return name.year;
+            //             }),
+            //             datasets: [{
+            //                 label: 
+            //             }]
+            //         }
+            //     }
+            // )
+        });
     }
     
     //都道府県一覧のチェックボックスを生成する配列を作るための処理
     useEffect(() => {
-        console.log("aaa");
-        const KEY = "sXkAFcfVBW9zVWIINTnMyFZGr9PdXOQ9vdFVFl5K";
-        const headers = new Headers();
-        headers.set("X-API-KEY", KEY);
-        const URL = `${props.endPoint}/api/v1/prefectures`;
+        const headers = setHeader(API_KEY);
+        const URL = `${END_POINT}/api/v1/prefectures`;
 
         fetch(URL, {headers: headers}).then(res => {
             return res.json();
@@ -23,22 +69,61 @@ export const CheckBoxs = (props) =>{
             setPrefs(json.result);
         });
 
+        //Chart.jsの初期処理だけしておくためにURLからlabelsを取得
+        const URL2 = `${END_POINT}/api/v1/population/composition/perYear?prefCode=1`;
+        fetch(URL2, {headers: headers}).then(res => {
+            return res.json();
+        })
+        .then(json => {
+            const thePref = json.result.data[0].data;
+            const graph = document.querySelector("#drawGraph").getContext("2d");
+
+            try{
+                myChart.destroy();
+            }
+            catch{
+                ;
+            }
+            
+            myChart = new Chart(graph, {
+                type: "line",
+                data: {
+                    labels: thePref.map(name => {return name.year})
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            id: "y-axis-1",
+                            type: "linear",
+                            position: "left",
+                        }],
+                    },
+                }
+            });
+        });
+
     }, [])
 
     return(
-        <div className="checkBoxes">
-            {
-                prefs.map((name, index) =>{
-                    return(
-                        <div className="one" key={name.prefCode.toString()}>
-                            <label htmlFor={name.prefCode.toString()}>{name.prefName}</label>
-                            <input type="checkbox" id={name.prefCode.toString()} onChange={() => showGraph(name.prefCode)} />
-                        </div>
-                        
-                    )
+        <>
+            <canvas className="populationGraph" id="drawGraph">
 
-                })
-            }
-        </div>
+            </canvas>
+            <div className="checkBoxes">
+                {
+                    prefs.map((name, index) =>{
+                        return(
+                            <div className="one" key={name.prefCode.toString()}>
+                                <label htmlFor={name.prefCode.toString()}>{name.prefName}</label>
+                                <input type="checkbox" id={name.prefCode.toString()} onChange={() => showGraph(name.prefCode)} />
+                            </div>
+                            
+                        )
+
+                    })
+                }
+            </div>
+        </>
+
     )
 }
